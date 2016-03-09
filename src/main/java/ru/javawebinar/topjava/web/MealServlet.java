2aa57ca28f3f972034b33ce04125768af762f2e3
vacoola1 <mail@vacoola.name>
2016-03-09 14:11:05 +0200

    test

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.web;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by r.vakulenko on 09.03.2016.
+ */
+public class MealServlet extends HttpServlet {
+
+}
